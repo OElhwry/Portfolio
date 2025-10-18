571375681dabfe0c6834ffa762f1b639dad6098b
OElhwry <omxrelx@gmail.com>
2025-10-19 00:43:27 +0100

    domain push (retry)

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // This tells Next.js to export static HTML
-  images: { unoptimized: true }, // Required for static exports
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "/", 
+  basePath: "",     
 };
 
 export default nextConfig;
