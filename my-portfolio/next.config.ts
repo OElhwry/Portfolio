fbcfbe7b9af8a3cbea674e8eca85a34c52b2d50e
OElhwry <omxrelx@gmail.com>
2025-10-19 02:24:41 +0100

    gh-page test

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -1,7 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  trailingSlash: true,
-};
+const nextConfig = { output: "export", images: { unoptimized: true }, trailingSlash: true };
 export default nextConfig;
