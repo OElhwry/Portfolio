d52f22c7581c4d1e66feeb04460375a5032f804c
OElhwry <omxrelx@gmail.com>
2026-04-16 23:39:06 +0100

    changing github pages -> vercel

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -2,9 +2,6 @@ import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
   outputFileTracingRoot: path.resolve(__dirname),
-  trailingSlash: true,
 };
 export default nextConfig;
